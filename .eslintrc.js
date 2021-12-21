b274a6977460165aa4aaaf7a01f542bad828a17e
Breno <bamachadu@gmail.com>
2021-12-21 17:25:33 -0300

    init project and configurations

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'eslint-config-prv',
+    'eslint-config-prv/jest',
+  ],
+  rules: {
+    radix: 'off',
+  },
+}
---
+++ jest.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/src/**/*.ts',
+    '!**/src/**/*.spec.ts',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: '<rootDir>/__coverage__',
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  roots: ['<rootDir>/test'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleDirectories: [
+    ".",
+    "src",
+    "node_modules"
+  ],
+  moduleNameMapper: {
+    '@common/(.*)': '<rootDir>/src/common/$1',
+    'data/(.*)': '<rootDir>/src/data/$1',
+    'domain/(.*)': '<rootDir>/src/domain/$1',
+    'main/(.*)': '<rootDir>/src/main/$1',
+    'presentation/(.*)': '<rootDir>/src/presentation/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1',
+  },
+  verbose: true,
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+}
